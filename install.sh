799140df2bbc9db43e71435aa36f92dc130fa2dd
Thomas Quartier <T.Quartier@connectgroup.com>
2015-07-16 08:27:45 +0200

    Updated install script with full paths

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-ln -s vimrc ~/.vimrc
-ln -s tmux.conf ~/.tmux.conf
-ln -s ycm_extra_conf.py ~/.ycm_extra_conf.py
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/ycm_extra_conf.py ~/.ycm_extra_conf.py
