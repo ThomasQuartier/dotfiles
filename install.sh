e6a36d93fa904f32ccd0cc6537ed86fe85d26d4c
Thomas Quartier <thomas.quartier@altran.com>
2016-05-18 15:28:31 +0200

    Update install script and add zshrc file

--- install.sh
+++ install.sh
@@ -1,4 +1,20 @@
-ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/ycm_extra_conf.py ~/.ycm_extra_conf.py
-cat ~/dotfiles/bashrc_append >> ~/.bashrc
+#!/bin/bash
+
+dir=~/.dotfiles
+olddir=~/dotfiles_old
+files="zshrc tmux.conf vimrc ycm_extra_conf.py"
+
+echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+mkdir -p $olddir
+echo "done"
+
+echo -n "Changing to the $dir directory ..."
+cd $dir
+echo "done"
+
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
